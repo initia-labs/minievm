b24107f331aeb75f0a889c7dd4b1178009655968
beer-1 <147697694+beer-1@users.noreply.github.com>
2024-05-10 17:30:57 +0900

    bump slinky to latest and use hash strategy

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -8,11 +8,13 @@ IBC_URL=github.com/cosmos/ibc-go
 IBC_V=v8
 INITIA_URL=github.com/initia-labs/initia
 OPINIT_URL=github.com/initia-labs/OPinit
+SLINKY_URL=github.com/skip-mev/slinky
 
 COSMOS_SDK_VERSION=$(cat ./go.mod | grep "$COSMOS_URL v" | sed -n -e "s/^.* //p")
 IBC_VERSION=$(cat ./go.mod | grep "$IBC_URL/$IBC_V v" | sed -n -e "s/^.* //p")
 INITIA_VERSION=$(cat ./go.mod | grep "$INITIA_URL v" | sed -n -e "s/^.* //p")
 OPINIT_VERSION=$(cat ./go.mod | grep "$OPINIT_URL v" | sed -n -e "s/^.* //p")
+SLINKY_VERSION=$(cat ./go.mod | grep "$SLINKY_URL v" | sed -n -e "s/^.* //p")
 
 mkdir -p ./third_party
 cd third_party
@@ -20,6 +22,7 @@ git clone -b $INITIA_VERSION https://$INITIA_URL
 git clone -b $OPINIT_VERSION https://$OPINIT_URL
 git clone -b $COSMOS_SDK_VERSION https://$COSMOS_URL
 git clone -b $IBC_VERSION https://$IBC_URL
+git clone -b $SLINKY_VERSION https://$SLINKY_URL
 cd ..
 
 # start generating
@@ -31,6 +34,7 @@ proto_dirs=$(find \
   ../third_party/ibc-go/proto/ibc \
   ../third_party/initia/proto \
   ../third_party/opinit/proto \
+  ../third_party/slinky/proto \
   -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   # generate swagger files (filter query files)
