117392887130702f8b79527ed6ef0cfc9c75b8ea
Hoon <48665813+sh-cha@users.noreply.github.com>
2024-05-24 13:50:26 +0900

    delete contract query limit config (#20)
    * delete contract query limit config
    
    * default 3,000,000

--- cmd/minitiad/config.go
+++ cmd/minitiad/config.go
@@ -40,6 +40,7 @@ func initAppConfig() (string, interface{}) {
 	//
 	// In simapp, we set the min gas prices to 0.
 	srvCfg.MinGasPrices = fmt.Sprintf("0%s", types.BaseDenom)
+	srvCfg.QueryGasLimit = 3000000
 
 	// Enable API and unsafe CORS (CORS allowed from any host)
 	srvCfg.API.Enable = true
