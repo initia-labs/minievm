1b2ee5dd685edc40c59a84e677fb3aba2550e046
beer-1 <beer-1@codingcats.xyz>
2024-10-21 11:30:35 +0900

    skip height 1

--- x/evm/abci.go
+++ x/evm/abci.go
@@ -13,5 +13,11 @@ import (
 // PreBlock track latest 256 block hashes
 func PreBlock(ctx sdk.Context, k *keeper.Keeper) (sdk.ResponsePreBlock, error) {
 	defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyEndBlocker)
-	return sdk.ResponsePreBlock{}, k.TrackBlockHash(ctx, uint64(ctx.BlockHeight()-1), common.BytesToHash(ctx.HeaderHash()))
+	height := uint64(ctx.BlockHeight())
+	if height <= 1 {
+		return sdk.ResponsePreBlock{}, nil
+	}
+
+	// current block header hash is created by the previous block, so we track it with height-1
+	return sdk.ResponsePreBlock{}, k.TrackBlockHash(ctx, height-1, common.BytesToHash(ctx.HeaderHash()))
 }
