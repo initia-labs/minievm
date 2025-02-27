20fdf0a2ac6697911175ed96d514bc3aca4a26c8
suha jin <89185836+djm07073@users.noreply.github.com>
2025-02-27 16:39:48 +0900

    test: make test setup with inf block gas meter

--- x/evm/keeper/context.go
+++ x/evm/keeper/context.go
@@ -62,12 +62,17 @@ func (k Keeper) buildBlockContext(ctx context.Context, defaultBlockCtx vm.BlockC
 		return vm.BlockContext{}, err
 	}
 
+	meter := sdkCtx.BlockGasMeter()
+	if meter == nil {
+		return vm.BlockContext{}, fmt.Errorf("no block gas meter exist")
+	}
+
 	return vm.BlockContext{
 		BlockNumber: defaultBlockCtx.BlockNumber,
 		Time:        defaultBlockCtx.Time,
 		Random:      defaultBlockCtx.Random,
 		BaseFee:     baseFee,
-		GasLimit:    sdkCtx.BlockGasMeter().Limit(),
+		GasLimit:    meter.Limit(),
 		CanTransfer: func(sd vm.StateDB, a common.Address, i *uint256.Int) bool {
 			if i == nil || i.IsZero() {
 				return true
