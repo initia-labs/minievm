c69c5cfa78f3b325b16585918165b80017fe8ca8
traviolus <42636319+traviolus@users.noreply.github.com>
2025-05-08 06:59:21 +0700

    feat(indexer): implement batch for cache store

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -10,7 +10,6 @@ import (
 	"cosmossdk.io/collections"
 	corestoretypes "cosmossdk.io/core/store"
 	"cosmossdk.io/log"
-	"cosmossdk.io/store/dbadapter"
 	snapshot "cosmossdk.io/store/snapshots/types"
 	storetypes "cosmossdk.io/store/types"
 	dbm "github.com/cosmos/cosmos-db"
@@ -85,7 +84,7 @@ type EVMIndexerImpl struct {
 	txConfig client.TxConfig
 	appCodec codec.Codec
 
-	store     *CacheStore
+	store     *CacheStoreWithBatch
 	evmKeeper *evmkeeper.Keeper
 
 	schema collections.Schema
@@ -128,7 +127,7 @@ func NewEVMIndexer(
 		cfg.IndexerCacheSize = evmconfig.DefaultIndexerCacheSize
 	}
 
-	store := NewCacheStore(dbadapter.Store{DB: db}, cfg.IndexerCacheSize)
+	store := NewCacheStoreWithBatch(db, cfg.IndexerCacheSize)
 	sb := collections.NewSchemaBuilderFromAccessor(
 		func(ctx context.Context) corestoretypes.KVStore {
 			// if there is prune store in context, use it
--- indexer/store.go
+++ indexer/store.go
@@ -2,23 +2,31 @@ package indexer
 
 import (
 	"context"
+	"sync"
 
 	corestoretypes "cosmossdk.io/core/store"
-	"cosmossdk.io/store"
-	cachekv "cosmossdk.io/store/cachekv"
+	"cosmossdk.io/store/cachekv"
+	"cosmossdk.io/store/dbadapter"
 	storetypes "cosmossdk.io/store/types"
-
-	bigcache "github.com/allegro/bigcache/v3"
+	"github.com/allegro/bigcache/v3"
+	dbm "github.com/cosmos/cosmos-db"
 )
 
-var _ corestoretypes.KVStore = (*CacheStore)(nil)
+var _ corestoretypes.KVStore = (*CacheStoreWithBatch)(nil)
 
-type CacheStore struct {
+const (
+	DefaultBatchFlushThreshold = 4 * 1024 * 1024 // 4MB
+)
+
+type CacheStoreWithBatch struct {
 	store storetypes.CacheKVStore
 	cache *bigcache.BigCache
+	batch dbm.Batch
+	db    dbm.DB
+	mtx   sync.Mutex
 }
 
-func NewCacheStore(store storetypes.KVStore, capacity int) *CacheStore {
+func NewCacheStoreWithBatch(db dbm.DB, capacity int) *CacheStoreWithBatch {
 	// default with no eviction and custom hard max cache capacity
 	cacheCfg := bigcache.DefaultConfig(0)
 	cacheCfg.Verbose = false
@@ -29,34 +37,35 @@ func NewCacheStore(store storetypes.KVStore, capacity int) *CacheStore {
 		panic(err)
 	}
 
-	return &CacheStore{
-		store: cachekv.NewStore(store),
+	return &CacheStoreWithBatch{
+		store: cachekv.NewStore(dbadapter.Store{DB: db}),
 		cache: cache,
+		db:    db,
 	}
 }
 
 // Get returns nil iff key doesn't exist. Errors on nil key.
-func (c CacheStore) Get(key []byte) ([]byte, error) {
+func (c *CacheStoreWithBatch) Get(key []byte) ([]byte, error) {
 	storetypes.AssertValidKey(key)
 
 	if value, err := c.cache.Get(string(key)); err == nil {
 		return value, nil
 	}
 
-	// get from store and write to cache
+	// if not in cache, get from kvstore
 	value := c.store.Get(key)
 	if value == nil {
 		return nil, nil
 	}
 
-	// ignore cache error
+	// cache for future reads, ignore error
 	_ = c.cache.Set(string(key), value)
 
 	return value, nil
 }
 
-// Has checks if a key exists. Errors on nil key.
-func (c CacheStore) Has(key []byte) (bool, error) {
+// Has checks if a key exists. Errors on a nil key.
+func (c *CacheStoreWithBatch) Has(key []byte) (bool, error) {
 	_, err := c.cache.Get(string(key))
 	if err == nil {
 		return true, nil
@@ -74,28 +83,84 @@ func (c CacheStore) Has(key []byte) (bool, error) {
 }
 
 // Set sets the key. Errors on nil key or value.
-func (c CacheStore) Set(key, value []byte) error {
+func (c *CacheStoreWithBatch) Set(key, value []byte) error {
 	storetypes.AssertValidKey(key)
 	storetypes.AssertValidValue(value)
 
-	// ignore cache error
-	_ = c.cache.Set(string(key), value)
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
 
+	// update both caches
+	_ = c.cache.Set(string(key), value)
 	c.store.Set(key, value)
 
-	return nil
+	// ensure batch is available
+	if c.batch == nil {
+		c.batch = c.db.NewBatch()
+	}
+
+	batchSizeAfter, err := c.estimateSizeAfterSetting(key, value)
+	if err != nil {
+		return err
+	}
+	if batchSizeAfter > DefaultBatchFlushThreshold {
+		c.Write()
+	}
+
+	// add to batch for persistence
+	return c.batch.Set(key, value)
 }
 
-// Delete deletes the key. Errors on nil key.
-func (c CacheStore) Delete(key []byte) error {
+// Delete deletes the key. Errors on a nil key.
+func (c *CacheStoreWithBatch) Delete(key []byte) error {
 	storetypes.AssertValidKey(key)
 
-	// ignore cache error
-	_ = c.cache.Delete(string(key))
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
 
+	// update both caches
+	_ = c.cache.Delete(string(key))
 	c.store.Delete(key)
 
-	return nil
+	// ensure batch is available
+	if c.batch == nil {
+		c.batch = c.db.NewBatch()
+	}
+
+	batchSizeAfter, err := c.estimateSizeAfterSetting(key, []byte{})
+	if err != nil {
+		return err
+	}
+	if batchSizeAfter > DefaultBatchFlushThreshold {
+		c.Write()
+	}
+
+	// add to batch for persistence
+	return c.batch.Delete(key)
+}
+
+// cacheIterator wraps a storetypes.Iterator to add caching
+type cacheIterator struct {
+	storetypes.Iterator
+	cache *bigcache.BigCache
+}
+
+// Value returns the value at the current position
+func (i *cacheIterator) Value() []byte {
+	key := i.Key()
+
+	// try cache first
+	if value, err := i.cache.Get(string(key)); err == nil {
+		return value
+	}
+
+	// get from the underlying iterator
+	value := i.Iterator.Value()
+	if value != nil {
+		_ = i.cache.Set(string(key), value)
+	}
+
+	return value
 }
 
 // Iterator iterates over a domain of keys in ascending order. End is exclusive.
@@ -104,8 +169,11 @@ func (c CacheStore) Delete(key []byte) error {
 // To iterate over entire domain, use store.Iterator(nil, nil)
 // CONTRACT: No writes may happen within a domain while an iterator exists over it.
 // Exceptionally allowed for cachekv.Store, safe to write in the modules.
-func (c CacheStore) Iterator(start, end []byte) (storetypes.Iterator, error) {
-	return c.store.Iterator(start, end), nil
+func (c *CacheStoreWithBatch) Iterator(start, end []byte) (storetypes.Iterator, error) {
+	return &cacheIterator{
+		Iterator: c.store.Iterator(start, end),
+		cache:    c.cache,
+	}, nil
 }
 
 // ReverseIterator iterates over a domain of keys in descending order. End is exclusive.
@@ -113,63 +181,36 @@ func (c CacheStore) Iterator(start, end []byte) (storetypes.Iterator, error) {
 // Iterator must be closed by caller.
 // CONTRACT: No writes may happen within a domain while an iterator exists over it.
 // Exceptionally allowed for cachekv.Store, safe to write in the modules.
-func (c CacheStore) ReverseIterator(start, end []byte) (storetypes.Iterator, error) {
-	return c.store.ReverseIterator(start, end), nil
+func (c *CacheStoreWithBatch) ReverseIterator(start, end []byte) (storetypes.Iterator, error) {
+	return &cacheIterator{
+		Iterator: c.store.ReverseIterator(start, end),
+		cache:    c.cache,
+	}, nil
 }
 
-func (c CacheStore) Write() {
-	c.store.Write()
+// Write writes the batch to the store.
+func (c *CacheStoreWithBatch) Write() {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+
+	if c.batch == nil {
+		return
+	}
+
+	_ = c.batch.Write()
+	_ = c.batch.Close()
+
+	// clear the batch after writing
+	c.batch = nil
 }
 
-// coreKVStore is a wrapper of Core/Store kvstore interface
-// Remove after https://github.com/cosmos/cosmos-sdk/issues/14714 is closed
-type coreKVStore struct {
-	kvStore storetypes.KVStore
-}
+// estimateSizeAfterSetting estimates the batch's size after setting a key / value
+func (c *CacheStoreWithBatch) estimateSizeAfterSetting(key []byte, value []byte) (int, error) {
+	currentSize, err := c.batch.GetByteSize()
+	if err != nil {
+		return 0, err
+	}
 
-// newCoreKVStore returns a wrapper of Core/Store kvstore interface
-// Remove once store migrates to core/store kvstore interface
-func newCoreKVStore(store storetypes.KVStore) corestoretypes.KVStore {
-	return coreKVStore{kvStore: store}
-}
-
-// Get returns nil iff key doesn't exist. Errors on nil key.
-func (store coreKVStore) Get(key []byte) ([]byte, error) {
-	return store.kvStore.Get(key), nil
-}
-
-// Has checks if a key exists. Errors on nil key.
-func (store coreKVStore) Has(key []byte) (bool, error) {
-	return store.kvStore.Has(key), nil
-}
-
-// Set sets the key. Errors on nil key or value.
-func (store coreKVStore) Set(key, value []byte) error {
-	store.kvStore.Set(key, value)
-	return nil
-}
-
-// Delete deletes the key. Errors on nil key.
-func (store coreKVStore) Delete(key []byte) error {
-	store.kvStore.Delete(key)
-	return nil
-}
-
-// Iterator iterates over a domain of keys in ascending order. End is exclusive.
-// Start must be less than end, or the Iterator is invalid.
-// Iterator must be closed by caller.
-// To iterate over entire domain, use store.Iterator(nil, nil)
-// CONTRACT: No writes may happen within a domain while an iterator exists over it.
-// Exceptionally allowed for cachekv.Store, safe to write in the modules.
-func (store coreKVStore) Iterator(start, end []byte) (store.Iterator, error) {
-	return store.kvStore.Iterator(start, end), nil
-}
-
-// ReverseIterator iterates over a domain of keys in descending order. End is exclusive.
-// Start must be less than end, or the Iterator is invalid.
-// Iterator must be closed by caller.
-// CONTRACT: No writes may happen within a domain while an iterator exists over it.
-// Exceptionally allowed for cachekv.Store, safe to write in the modules.
-func (store coreKVStore) ReverseIterator(start, end []byte) (store.Iterator, error) {
-	return store.kvStore.ReverseIterator(start, end), nil
+	// add 100 here just to overcompensate for overhead
+	return currentSize + len(key) + len(value) + 100, nil
 }
