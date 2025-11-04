7f25349664c3d2fca9febe00307c2c078ba91d14
beer-1 <147697694+beer-1@users.noreply.github.com>
2025-11-04 16:49:01 +0900

    impl eip-7966 (#262)

--- jsonrpc/backend/errors.go
+++ jsonrpc/backend/errors.go
@@ -23,3 +23,37 @@ var (
 	errInvalidPercentile = errors.New("invalid reward percentile")
 	errRequestBeyondHead = errors.New("request beyond head block")
 )
+
+// TimeoutError indicates that the request has timed out.
+type TimeoutError struct {
+	msg string
+}
+
+func NewTimeoutError(msg string) *TimeoutError {
+	return &TimeoutError{msg: msg}
+}
+
+func (e *TimeoutError) Error() string {
+	return "jsonrpc timeout error: " + e.msg
+}
+
+func (e *TimeoutError) ErrorCode() int {
+	return 4
+}
+
+// ReadinessError indicates that the backend is not ready to process requests.
+type ReadinessError struct {
+	msg string
+}
+
+func NewReadinessError(msg string) *ReadinessError {
+	return &ReadinessError{msg: msg}
+}
+
+func (e *ReadinessError) Error() string {
+	return "jsonrpc readiness error: " + e.msg
+}
+
+func (e *ReadinessError) ErrorCode() int {
+	return 5
+}
